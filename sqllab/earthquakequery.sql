9478d7e4a5834e0540798a134a35eab3c29806eb
bjones0218 <blake28jones@gmail.com>
2024-04-10 10:17:50 -0500

    new queries

--- sqllab/earthquakequery.sql
+++ sqllab/earthquakequery.sql
@@ -2,3 +2,20 @@ select quakedate, quaketime, place
 from earthquakes
 where mag > 5 and latitude between 50 and 100 
 ;
+
+select quakedate, quaketime, place
+from earthquakes
+where mag = 1
+;
+
+with quake_no as(
+	select place, count(id) quakeCount
+	from earthquakes
+	group by place	
+)
+
+select q.place, q.quakeCount
+from quake_no q
+join (select max(quakeCount) maxQuakes
+		from quake_no) as mq
+	on mq.maxQuakes = q.quakeCount
