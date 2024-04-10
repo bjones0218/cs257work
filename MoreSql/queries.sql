c67e02c7ebe502948412d92e9b362e92e5ce8fa3
bjones0218 <blake28jones@gmail.com>
2024-04-10 10:58:38 -0500

    query fix

--- MoreSql/queries.sql
+++ MoreSql/queries.sql
@@ -3,6 +3,6 @@ select t.city, (t.pop / s.pop) as state_pop_proportion
 from topCities t
 join statePop s
 	on s.state = t.state
-order by state_pop_proportion desc
+order by state_pop_proportion
 limit 10
 ;
