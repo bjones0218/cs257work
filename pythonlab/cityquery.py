88c2f1b4cf133a523bc7c888606691e65776fad2
bjones0218 <blake28jones@gmail.com>
2024-04-15 10:55:31 -0500

    Query two written

--- pythonlab/cityquery.py
+++ pythonlab/cityquery.py
@@ -20,9 +20,41 @@ def runQueryOne():
 			print("Northfield is not in this database.")
 		else:
 			print("The latitude is " + rows[0][3] + " and the longitude is " + rows[0][4])
+		
+		return True
 	else:
 		print("Problem with connection")
+		return False
 
-	return True
 
-runQueryOne()
+
+def runQueryTwo():
+	conn = psycopg2.connect(
+		host = "localhost",
+		port = 5432,
+		database = "jonesb2",
+		user = "jonesb2",
+		password = "card254cup")
+	
+	if conn is not None:
+		cur = conn.cursor()
+
+		sql = "select c.city from topCities c join (select max(pop) as maxPop from topCities) as m on c.pop = m.maxPop"
+
+		cur.execute(sql)
+		rows = cur.fetchall()
+
+		if len(rows) == 0:
+			print("There is a problem with your query")
+		else:
+			for each in rows:
+				print(each[0])
+		
+		return True
+	else:
+		print("Problem with connection")
+		return False
+
+
+# runQueryOne()
+runQueryTwo()
