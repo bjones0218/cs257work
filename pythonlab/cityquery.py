236ceea564295a5d76aa830548d3c7706ff84b3d
bjones0218 <blake28jones@gmail.com>
2024-04-15 20:47:00 -0500

    query four written

--- pythonlab/cityquery.py
+++ pythonlab/cityquery.py
@@ -82,6 +82,60 @@ def runQueryThree():
 		print("Problem with connection")
 		return False
 
+def runQueryFour():
+	conn = psycopg2.connect(
+		host = "localhost",
+		port = 5432,
+		database = "jonesb2",
+		user = "jonesb2",
+		password = "card254cup")
+	
+	if conn is not None:
+		cur = conn.cursor()
+
+		sql = '''with maxNorth as (
+					select max(lat) as mostNorth 
+					from topCities
+				),
+				maxSouth as (
+					select min(lat) as mostSouth
+					from topCities
+				),
+				maxEast as (
+					select min(lon) as mostEast
+						from topCities
+				), 
+				maxWest as (
+					select max(lon) as mostWest
+						from topCities
+				), 
+				select city 
+					from topCities 
+					join maxNorth 
+						on maxNorth.mostNorth = topCities.lat 
+					join maxSouth 
+						on maxSouth.mostSouth = topCities.lat 
+					join maxEast 
+						on maxEast.mostEast = topCities.lon 
+					join maxWest 
+						on maxWest.mostWest = topCities.lon
+				;'''
+
+		cur.execute(sql)
+		rows = cur.fetchall()
+
+		if len(rows) == 0:
+			print("There is a problem with your query")
+		else:
+			for each in rows:
+				print(each[0])
+		
+		return True
+	else:
+		print("Problem with connection")
+		return False
+
 # runQueryOne()
 # runQueryTwo()
 runQueryThree()
+# runQueryFour()
