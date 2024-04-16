9671b2d5272e9a3dd3b9194e5c1a97c02d56aace
bjones0218 <blake28jones@gmail.com>
2024-04-16 12:01:43 -0500

    query testing

--- pythonlab/cityquery.py
+++ pythonlab/cityquery.py
@@ -109,17 +109,19 @@ def runQueryFour():
 					select min(lon) as mostWest
 						from topCities
 				)
-				select topCities.city 
-					from topCities 
-					join maxNorth 
-						on maxNorth.mostNorth = topCities.lat 
-					join maxSouth 
-						on maxSouth.mostSouth = topCities.lat 
-					join maxEast 
-						on maxEast.mostEast = topCities.lon 
-					join maxWest 
-						on maxWest.mostWest = topCities.lon
+				select * from maxNorth
 				;'''
+
+					# select topCities.city 
+					# from topCities 
+					# join maxNorth 
+					# 	on maxNorth.mostNorth = topCities.lat 
+					# join maxSouth 
+					# 	on maxSouth.mostSouth = topCities.lat 
+					# join maxEast 
+					# 	on maxEast.mostEast = topCities.lon 
+					# join maxWest 
+					# 	on maxWest.mostWest = topCities.lon
 
 		cur.execute(sql)
 		rows = cur.fetchall()
