80e5ded54587ebe9266c28826e881a10d1bf6541
bjones0218 <blake28jones@gmail.com>
2024-04-22 10:41:02 -0500

    database use query added

--- flaskLab/flask_test.py
+++ flaskLab/flask_test.py
@@ -1,4 +1,5 @@
 import flask
+import psycopg2
 
 app = flask.Flask(__name__)
 
@@ -20,6 +21,35 @@ def my_color(word1):
 def my_sum(num1, num2):
     sum = int(num1) + int(num2)
     return str(sum)
+
+@app.route('/pop/<state>')
+def get_pop(state):
+    conn = psycopg2.connect(
+		host = "localhost",
+		port = 5432,
+		database = "jonesb2",
+		user = "jonesb2",
+		password = "card254cup")
+    
+	state = state.lower()
+    
+	if conn is not None:
+		cur = conn.cursor()
+
+		sql = '''select state, pop
+					from statePop
+					where lower(code) = %(state)s
+					;'''
+
+		cur.execute(sql, {"state":state})
+		rows = cur.fetchall()
+
+		if len(rows) == 0:
+			return "You did not enter a valid state"
+		else:
+			return str(rows[0][1])
+	else:
+		return "Problem with connection"
 
 if __name__ == '__main__':
     my_port = 5116
