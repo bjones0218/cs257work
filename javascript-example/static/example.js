0fd57be0233793fac5eb65356b98a6a7496e2301
bjones0218 <blake28jones@gmail.com>
2024-05-08 10:28:56 -0500

    renamed so delete files

--- javascript-example/static/example.js
+++
@@ -1,5 +0,0 @@
-function changeColor() {
-  the_heading = document.getElementById("hello");
-  the_heading.style.color = "red";
-  console.log("I just changed the color to: " + the_heading.style.color)  
-}
